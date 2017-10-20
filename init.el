1d4cc56b94bd2d755a0b829cede97274b1744f45
TomBosmans <t.bosse@hotmail.com>
2017-10-20 12:45:32 +0200

    This is not the commit you are looking for

---
+++ init.el
@@ -0,0 +1,51 @@
+(require 'package)
+
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode coffee-mode projectile solarized-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(set-default-font "Monaco 12")
+(load-theme 'solarized-light t)
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; Coffee mode: set tab of 2 spaces
+
+; autosave files in tmp dir
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+
+;; PJasper can't think of a single time this has helped him, so we trust him.
+(setq make-backup-files nil)
+
+(require 'ido)
+(ido-mode t)
+(setq ido-enable-flex-matching t) ; case insensitive matching
+(add-to-list 'ido-ignore-files "\\.DS_Store")
+(setq ido-create-new-buffer 'always) ; always create a new buffer with Ido
+(setq ido-use-virtual-buffers t)
