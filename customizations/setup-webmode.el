08414b29c872009d54d71684dfa51ad176f4156f
TomBosmans <t.bosse@hotmail.com>
2017-11-17 16:30:32 +0100

    added the webmode file

---
+++ customizations/setup-webmode.el
@@ -0,0 +1,21 @@
+(require 'web-mode)
+(require 'projectile)
+(add-hook 'projectile-mode-hook 'projectile-rails-on)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . sass-mode))
+; ruby
+(setq auto-mode-alist (cons '("Rakefile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Capfile" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.rake" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.god" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.ru" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.gemspec" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Gemfile" . ruby-mode) auto-mode-alist))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
