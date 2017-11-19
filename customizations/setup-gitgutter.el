db658243624c5e4317294c5a8f684332d1648ccd
TomBosmans <t.bosse@hotmail.com>
2017-11-19 10:51:51 +0100

    More Tie fighters!

--- customizations/setup-gitgutter.el
+++ customizations/setup-gitgutter.el
@@ -4,6 +4,8 @@
 (global-git-gutter-mode +1)
 
 (setq-default left-fringe-width  20)
+
+;; Tie fighter
 (fringe-helper-define 'git-gutter-fr:added nil
   "X......X"
   "X......X"
@@ -14,22 +16,24 @@
   "X......X"
   "X......X")
 
+;; Tie bomber
 (fringe-helper-define 'git-gutter-fr:deleted nil
+  "........"
+  ".X....X."
   "X......X"
-  "X......X"
-  "X..XX..X"
   "XXXXXXXX"
-  "X.XXXX.X"
-  "X..XX..X"
+  "X.X..X.X"
   "X......X"
-  "X......X")
+  ".X....X."
+  "........")
 
+;; Tie Advanced x1
 (fringe-helper-define 'git-gutter-fr:modified nil
-  "X......X"
+  ".X....X."
   "X......X"
   "X..XX..X"
   "XXXXXXXX"
   "X.XXXX.X"
   "X..XX..X"
   "X......X"
-  "X......X")
+  ".X....X.")
