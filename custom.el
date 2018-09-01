7f003df97961f9ca9b3650646a6f2cf45db7980e
TomBosmans <t.bosse@hotmail.com>
2018-09-01 15:22:11 +0200

    remove custom.el from gitignore

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (rjsx-mode yasnippet yaml-mode web-mode use-package solarized-theme smex smartparens sass-mode robe reveal-in-osx-finder rainbow-mode projectile prodigy powerline popwin pallet nyan-mode neotree multiple-cursors mmm-mode minitest markdown-preview-mode markdown-preview-eww magit idle-highlight-mode htmlize highlight-indentation handlebars-mode git-gutter-fringe flycheck-color-mode-line flycheck-cask fill-column-indicator expand-region exec-path-from-shell emojify emmet-mode drag-stuff doom-themes dashboard crystal-mode coffee-mode ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
